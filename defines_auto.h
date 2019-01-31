bf1718359fc6473a07a676d023935a3ba159bf06
Radek Slabý <jakjinak@seznam.cz>
2019-01-31 21:22:47 +0100

    add descriptions

--- defines_auto.h
+++ defines_auto.h
@@ -6,6 +6,20 @@
 // https://gcc.gnu.org/onlinedocs/cpp/Common-Predefined-Macros.html
 // https://msdn.microsoft.com/en-us/library/b0084kay.aspx
 
+/*! JJ_COMPILER_GCC
+Defined if building with GCC/G++. */
+/*! JJ_COMPILER_MSVC
+Defined if building with MS Visual Studio. */
+/*! JJ_COMPILER_VERSION_MAJOR
+Expands to compilers major version. */
+/*! JJ_COMPILER_VERSION_MINOR
+Expands to compilers minor version. */
+/*! JJ_COMPILER_VERSION_BUILD
+Expands to compilers build revision. */
+
+/*! JJ_FUNC 
+Expands to the compilers symbol for "current function decorated". */
+
 #if defined(__GNUC__)
 #define JJ_COMPILER_GCC
 #define JJ_COMPILER_VERSION_MAJOR __GNUC__
@@ -23,6 +37,12 @@
 #define JJ_FUNC __FUNCSIG__
 #endif
 
+/*! JJ_OS_WINDOWS
+Defined if building on native Windows. */
+/*! JJ_OS_CYGWIN
+Defined if building under Cygwin. */
+/*! JJ_OS_LINUX
+Defined if building under Linux. */
 #if defined(_WIN32)
 #define JJ_OS_WINDOWS
 #define JJ_USE_WSTRING
@@ -30,11 +50,12 @@
 #define JJ_OS_CYGWIN
 #define JJ_USE_STRING
 #else
-#define JJ_OS_UNIX_LINUX
 #define JJ_OS_LINUX
 #define JJ_USE_STRING
 #endif
 
+/*! JJ_USE_CODECVT
+Defined if string conversion functions are available (narrow<->wide strings). */
 #if defined(JJ_COMPILER_MSVC) || ( __GNUC__ > 5 ) || (__GNUC__ == 5 && (__GNUC_MINOR__ > 1 ) )
 // this is only supported on windows (vs2017) or with g++ newer than 5.1
 #define JJ_USE_CODECVT
--- idGenerator.h
+++ idGenerator.h
@@ -1,24 +1,33 @@
 #ifndef JJ_ID_GENERATOR_H
 #define JJ_ID_GENERATOR_H
 
-
+/*! A simple wrapper over an integer id. */
 class idHolder_t
 {
-    int id_;
+    int id_; //!< the id
 public:
+    /*! Ctor - defaults id to -1 = not set */
     idHolder_t() : id_(-1) {}
+    /*! Ctor - initializes to given id */
     idHolder_t(int id) : id_(id) {}
+    /*! Changes the id to given one */
     void set_id(int id) { id_ = id; }
+    /*! Returns the stored id. */
     int get_id() const { return id_; }
 };
 
+/*! A simple generator of unique integer ids.
+Only an instance guarantees "uniqueness". */
 class idGenerator_t
 {
-    int idgen_;
+    int idgen_; //!< counter of "uniqueness"
 protected:
+    /*! Ctor */
     idGenerator_t() : idgen_(0) {}
+    /*! Copy ctor - copies do not share state and may generate same ids */
     idGenerator_t(const idGenerator_t&) : idgen_(0) {}
 public:
+    /*! Generates unique id. */
     int get_an_id() { return ++idgen_; }
 };
 
